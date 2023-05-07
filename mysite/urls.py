43119ef41a1f12bcba0cacdbc48b101544b548b4
松岡侑幹 <yuki@yukinoMacBook-Air.local>
2023-05-08 08:19:49 +0900

    0

--- mysite/urls.py
+++ mysite/urls.py
@@ -25,15 +25,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from . import views
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("flower/", include("flower.urls")),
-    path('', views.home, name='home'),
-    path('admin/', admin.site.urls),
-    path('flower/', views.flower, name='flower'),
-    path('media/(?P<path>.*)', serve, {'document_root': settings.MEDIA_ROOT}),
     #path("admin/", admin.site.urls),
     #path("flower/", include("flower.urls")),
 ]
--- mysite/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    # ホームページのビューの実装
-    return render(request, 'home.html')
